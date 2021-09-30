29dce636e06d29ddea94053a130fa17bf4ea5c53
Michael Jackson <michael.lenard.jackson@gmail.com>
2021-09-29 21:57:06 -0500

    Add header and tasks components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from 'react';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
+const App = () => {
+  const [tasks, setTask] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Tattoo Appointment',
+        day: 'Feb 15th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'TK Appointment',
+        day: 'Feb 25th at 2:30pm',
+        reminder: false,
+    }
+])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
