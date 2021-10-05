11adc04c3fbd10f8687a2ed135d792d649d20070
Michael Jackson <michael.lenard.jackson@gmail.com>
2021-10-04 21:52:45 -0500

    Add functionality of Add button toggling form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState} from 'react';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/addTask'
 
 const App = () => {
-  const [tasks, setTask] = useState([
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
     {
         id: 1,
         text: 'Doctors Appointment',
@@ -24,21 +26,30 @@ const App = () => {
     }
 ])
 
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask ]) // Outputs new task to list
+  console.log(newTask)
+}
+
 // Delete Task
 const deleteTask = (id) => {
- setTask(tasks.filter((task) => task.id !== id))
+ setTasks(tasks.filter((task) => task.id !== id))
 }
 
 // Toggle reminder
 const toggleReminder = (id) => {
-  setTask(tasks.map((task) => task.id === id 
+  setTasks(tasks.map((task) => task.id === id 
   ? { ...task, reminder: !task.reminder } : task))
 }
 
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd ={() => setShowAddTask(!showAddTask) }/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? ( 
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
