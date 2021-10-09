8322905387138dd66a41a75994e8064ff9693eb4
Michael Jackson <michael.lenard.jackson@gmail.com>
2021-10-08 22:32:33 -0500

    Add states to add button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title, onAdd }) => {
+const Header = ({ title, onAdd, showAdd }) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button text='Add' onClick={onAdd} />
+            <Button 
+                color={showAdd ? 'pink' : 'lightblue'} 
+                text={showAdd ? 'Close' : 'Add'} 
+                onClick={onAdd} 
+            />
         </header>
     )
 } 
